a92ab34d133cc85990bcaf3b0cb3f339df3bd560
akozma89 <14072413+akozma89@users.noreply.github.com>
2026-07-04 22:23:31 +0000

    Update PR Flow to v1.7.0

--- Casks/pr-flow.rb
+++ Casks/pr-flow.rb
@@ -1,7 +1,7 @@
 cask "pr-flow" do
-  version "1.6.2"
-  sha256 arm:   "ef5432cec8329f7fbad7c0c183e022105716965787a7b8853d6ea7bf9d99d789",
-         intel: "6813798b4318dd8c83fd75d4b9e6974f70dae06fd2eb4fc0178cff42ebf1e762"
+  version "1.7.0"
+  sha256 arm:   "e64f81b0bfd23f2ea16326875f2481a94be2c88eb49644eae159a9ad36c84077",
+         intel: "c220a529ba8e1833549dfe224e8ef4f3f9937d32e87d6a569c23ed315ee8adb1"
 
   on_arm do
     url "https://github.com/akozma89/pr-flow-releases/releases/download/v#{version}/PR-Flow-#{version}-arm64.dmg"
