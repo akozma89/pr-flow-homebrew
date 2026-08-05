286daeb0e7fe70ed397c81464715a3743d338e90
akozma89 <14072413+akozma89@users.noreply.github.com>
2026-08-05 21:04:13 +0000

    Update PR Flow to v1.11.1

--- Casks/pr-flow.rb
+++ Casks/pr-flow.rb
@@ -1,14 +1,8 @@
 cask "pr-flow" do
-  version "1.11.0"
-  sha256 arm:   "d7c6a358c497b4d765bfcb1714ec207511804a86926ee82c16dae0bef1fdbf0d",
-         intel: "e766887fb6acadd32492fc0295b3e8e66138604967d1ab3b2669edaa75d6e939"
+  version "1.11.1"
+  sha256 "2e3a30eefe2756d4b5a6962e53e48c0bd0236716f62d039e3a2c601c5a62ce4f"
 
-  on_arm do
-    url "https://github.com/akozma89/pr-flow-releases/releases/download/v#{version}/PR-Flow-#{version}-arm64.dmg"
-  end
-  on_intel do
-    url "https://github.com/akozma89/pr-flow-releases/releases/download/v#{version}/PR-Flow-#{version}.dmg"
-  end
+  url "https://github.com/akozma89/pr-flow-releases/releases/download/v#{version}/PR-Flow-#{version}-universal.dmg"
 
   name "PR Flow"
   desc "Menu-bar PR review assistant for GitHub & Gerrit"
