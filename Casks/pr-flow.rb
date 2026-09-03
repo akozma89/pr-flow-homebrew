e931ec46aa4ddf9afbe93639adadb30662267ffa
akozma89 <14072413+akozma89@users.noreply.github.com>
2026-09-03 18:23:03 +0000

    Update PR Flow to v1.15.0

--- Casks/pr-flow.rb
+++ Casks/pr-flow.rb
@@ -1,6 +1,6 @@
 cask "pr-flow" do
-  version "1.14.0"
-  sha256 "a2bf21f894e60b123dea9a93b97fbc42b2a8a322c3c443784b139196fe2c26a2"
+  version "1.15.0"
+  sha256 "352409a7c57ed40a5c60f4d8d423ee4f3118d7aeb98727b2f8a0bf45335d8226"
 
   url "https://github.com/akozma89/pr-flow-releases/releases/download/v#{version}/PR-Flow-#{version}-universal.dmg"
 
